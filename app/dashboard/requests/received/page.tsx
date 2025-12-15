5f44de74ad9442fd93183a1285e1805b3e0dacb8
Mustafa Salim <73708902+Trustafa@users.noreply.github.com>
2025-12-15 16:04:10 +0400

    fix: wrap pages that use useSearchParams to render client side only

--- app/dashboard/requests/received/page.tsx
+++ app/dashboard/requests/received/page.tsx
@@ -1,13 +1,14 @@
-'use client';
-
 import RequestsScreen from '@/app/_screens/RequestsScreen';
+import { Suspense } from 'react';
 
 export default function ReceivedRequestsPage() {
   return (
-    <RequestsScreen
-      baseRoute="/dashboard/requests/received"
-      title="Received Requests"
-      requestType="Received"
-    />
+    <Suspense>
+      <RequestsScreen
+        baseRoute="/dashboard/requests/received"
+        title="Received Requests"
+        requestType="Received"
+      />
+    </Suspense>
   );
 }
--- app/dashboard/requests/sent/page.tsx
+++ app/dashboard/requests/sent/page.tsx
@@ -1,13 +1,14 @@
-'use client';
-
 import RequestsScreen from '@/app/_screens/RequestsScreen';
+import { Suspense } from 'react';
 
 export default function SentRequestsPage() {
   return (
-    <RequestsScreen
-      baseRoute="/dashboard/requests/sent"
-      title="Sent Requests"
-      requestType="Sent"
-    />
+    <Suspense>
+      <RequestsScreen
+        baseRoute="/dashboard/requests/sent"
+        title="Sent Requests"
+        requestType="Sent"
+      />
+    </Suspense>
   );
 }
